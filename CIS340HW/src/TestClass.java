0ef9580dfa20cd907d4013eddd4f41a5db85f694
henryliu <henryliu@COBD0WXM.Busdom.ColoState.EDU>
2018-01-25 16:58:44 -0700

    test commit

--- CIS340HW/src/TestClass.java
+++ CIS340HW/src/TestClass.java
@@ -1,9 +1,12 @@
+import javax.swing.*;
 
 public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// test the commit and push
+		JOptionPane.showConfirmDialog(null, "this is to test changes");
 	}
 
 }
